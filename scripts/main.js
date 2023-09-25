7d6af589c9cd1db1343ac90148254db245c1efad
Manuela Gomez <manu.gomez.e@gmail.com>
2023-09-24 21:47:03 -0500

    created functions and style improve

---
+++ scripts/main.js
@@ -0,0 +1,52 @@
+/*------------DOM HANDLE-------------- */
+const form = document.querySelector('.modal');
+const addTask = document.getElementById('add-task');
+const tasksGroup = document.getElementById('to-do-tasks');
+let priorityField;
+
+/*------------------------------------ */
+let tasks=[];
+
+tasks=[
+    {
+        'task':'arreglar la bicicleta',
+        'priority':'low',
+        'deadline':'2024-10-10',
+        'note':'',
+    },
+]
+
+addTask.addEventListener("click",()=>{
+    form.style.display='flex';
+})
+    
+// -------------------Event listeners/functions-----------------------
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    taskField = document.getElementById('task');
+    dateField = document.getElementById('date');
+    noteField = document.getElementById('note');
+    priorityField = document.querySelector('.priority:checked');
+
+    let newTask={
+        'task':taskField.value,
+        'priority':priorityField.value,
+        'deadline':dateField.value,
+        'note': noteField.value
+    }
+
+    tasks.push(newTask);
+    closeModal();
+    });
+
+
+function closeModal(){
+    form.style.display='none';
+    if(priorityField){
+        taskField.value='';
+        priorityField.checked=false;
+        dateField.value='';
+        noteField.value='';
+    }
+}
---
+++ scripts/task.js
@@ -0,0 +1,10 @@
+let %template;
+
+function task(taksObj){
+    priorityField(taksObj.priority);
+
+    $template =`<div> 
+    <h2>${taskObj.task}</h2> 
+    
+    </div>`;
+}
